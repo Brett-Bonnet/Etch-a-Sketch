2ee1adb857f3322b8fa35de87ca9a10b6d1538ac
Brett Bonnet <brettbonnet4@gmail.com>
2019-01-15 07:06:19 -0500

    Created toggleRandomColor func

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ const container = document.querySelector('#container');
 const boxdiv = document.getElementsByClassName('boxdiv');
 const reset = document.getElementById('reset');
 const randomColors = document.getElementById('random-colors')
-let defaultSize = 10000;
+let defaultSize = 100;
 const rndButton = document.getElementsByClassName('highlight')
-
+let randomColorMode = false;
 
 var cell = document.querySelectorAll('.boxdiv');
 
 function getGrid(num) {
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < Math.pow(num, 2); i++) {
     const div = document.createElement('div');
     div.classList.add('boxdiv')
-    const specs = Math.sqrt(num);
+    const toSquare = Math.pow(num, 2);
+    const specs = Math.sqrt(toSquare);
     height = 600 / specs;
     width = 600 / specs;
     div.style.width = `${height}px`;
@@ -23,10 +24,13 @@ function getGrid(num) {
   divs.forEach(div => div.addEventListener('mouseover', changeColor));
 };
 
+
 function changeColor(e){
-	
+
 e.target.style.backgroundColor = 'black';
+
 }
+
 
 function clearBox() {
   const divs = document.querySelectorAll('.boxdiv');
@@ -41,33 +45,24 @@ while (myNode.firstChild) {
 }
 
 function resetGrid() {
-let howMany = prompt("How many squares would you like?");
+let howMany = prompt("How many squares per side would you like?");
+if (isNaN(howMany) || Math.sign(howMany) !== 1) {
+  alert("You must enter a proper number!");
+  window.location.reload();
+}
 rmvGrid();
 getGrid(howMany)
-}
-
-const randomColorGen = function () {
- 
- const divs = document.querySelectorAll('.boxdiv');
- divs.forEach(div => div.addEventListener('mouseover', randoColor))	
- 	
-}
+};
 
 
-function randoColor(e) {
-  
-  e.target.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);	 
-}
-
-
-
-randomColors.addEventListener('click', randomColorGen);
-randomColors.addEventListener('click', function() {
-const random = document.querySelector("#random-colors")	
-random.classList.toggle('highlight');		
-});
 
 reset.addEventListener('click', resetGrid);
+randomColors.addEventListener('click', toggleRandomColor);
+randomColors.addEventListener('click', function() {
+      const random = document.querySelector("#random-colors")	
+      random.classList.toggle('highlight');
+  });
+
 
 
 const onLoad = () => {
@@ -75,3 +70,34 @@ const onLoad = () => {
 }
 
 onLoad();
+
+
+
+
+
+
+
+
+function toggleRandomColor(e) {
+
+      
+  if (!randomColorMode) {
+      const divs = document.querySelectorAll('.boxdiv');
+      divs.forEach(div => div.addEventListener('mouseover', function() {
+
+      e.target.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);        
+      randomColorMode = true;
+
+      }))           		
+} else {
+  const divs = document.querySelectorAll('.boxdiv');
+  divs.forEach(div => div.addEventListener('mouseover', function() {
+
+  e.target.style.backgroundColor = 'black';        
+  randomColorMode = false
+
+       }));
+
+	}
+
+}
